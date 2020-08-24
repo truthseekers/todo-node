2c9800b12d8a407e02c77103d28c2d4d5832b79a
John Curry <fastpenguin91@gmail.com>
2020-08-23 20:20:10 -0700

    returning userId to context in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const { GraphQLServer } = require("graphql-yoga");
 const { PrismaClient } = require("@prisma/client");
+const jwt = require("jsonwebtoken");
 const prisma = new PrismaClient();
-const { APP_SECRET, getUserId, createToken } = require("./utils");
+const {
+  APP_SECRET,
+  getUserId,
+  createToken,
+  getUserIdFromToken,
+} = require("./utils");
 
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
@@ -24,6 +30,24 @@ const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
   resolvers,
   context: (request) => {
+    const userId = getUserIdFromToken(request);
+
+    console.log("userId from the index.js. were good so far");
+    console.log(userId);
+    // console.log("yayayayaya userId: ");
+    // console.log(userId);
+
+    //const Authorization = request.request.get("Authorization");
+    //console.log("Auth in index.js");
+    //console.log(Authorization);
+
+    //if (Authorization) {
+    //  const token = Authorization.replace("Bearer ", "");
+    //  const { userId } = jwt.verify(token, APP_SECRET);
+    // console.log("userId in conditional in index.js");
+    // console.log(userId);
+    //}
+
     return {
       ...request,
       prisma,
--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,50 @@
 const jwt = require("jsonwebtoken");
 const APP_SECRET = "GraphQL-is-aw3some";
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 console.log("hello from utils!");
 
 function createToken(user) {
-  return jwt.sign(user, APP_SECRET);
+  return jwt.sign(user, APP_SECRET); // this is how the return value of the jwt gets populated with user info. You're storing the data into the token here and it gets spit back out when you
+  // query for the web token. Because the "user" object is only an id... that's all I get back
 }
 
+const getUserIdFromToken = (arg) => {
+  const Authorization = arg.request.get("Authorization");
+
+  // console.log("Authorization in utils.js");
+  // console.log(Authorization);
+
+  if (Authorization) {
+    const token = Authorization.replace("Bearer ", "");
+    const { userId } = jwt.verify(token, APP_SECRET);
+    // console.log("userId from jwt in utils!");
+    // console.log(userId);
+    return userId;
+  }
+
+  //   if (Authorization) {
+  //     console.log("in Auth conditional of getUserIdFromToken");
+  //     const token = Authorization.replace("Bearer ", "");
+  //     // const userObj = jwt.verify(token, APP_SECRET);
+  //     const { userId } = jwt.verify(token, APP_SECRET);
+  //     //console.log("user obj is.... STILL ");
+  //     // console.log(userObj);
+  //     // return prisma.user.findOne({
+  //     //   where: {
+  //     //     id: userId,
+  //     //   },
+  //     // });
+  //     return userId;
+  //     // return userObj;
+  //   }
+};
+
 function getUserId(context) {
-  console.log("getUserId");
+  // console.log("getUserId");
   const Authorization = context.request.get("Authorization");
+  // console.log(Authorization);
   if (Authorization) {
     const token = Authorization.replace("Bearer ", "");
     const { userId } = jwt.verify(token, APP_SECRET);
@@ -28,4 +63,5 @@ module.exports = {
   APP_SECRET,
   getUserId,
   createToken,
+  getUserIdFromToken,
 };
